a1c6667606fbdd56ff330ecf9ef76efd970fbeea
rahman <ainurahman03@gmail.com>
2020-07-18 19:49:12 +0700

    first commit

---
+++ tipe_data.py
@@ -0,0 +1,16 @@
+#tipe data : angka satuan(integer)
+data_int =4
+# untuk mengetahui tipe data dalam suatu variabel menggunkana func type
+print("nilai: ",data_int,"tipe data: ",type(data_int))
+
+# tipe data :decimal (float)
+data_float = 0.4
+print("nilai: ",data_float,"tipe data: ",type(data_float))
+
+#tipe data : kumpulan karakter (string)
+data_str = "0.4"
+print("nilai: ",data_str,"tipe data: ",type(data_str))
+
+#tipe data : boolean true/false
+data_bool = True
+print("nilai: ",data_bool,"tipe data: ",type(data_bool))
---
+++ variabel.py
@@ -0,0 +1,3 @@
+#variabel adalah tempat menyimpan data
+a = 10 #a sebagai variabel , = merupakan assingment dan 10 berupa value
+print("nilai a = ",a)
